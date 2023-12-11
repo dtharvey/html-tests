444de5330751ecb68da2a7535c6150f493e60118
dtharvey <harvey@depauw.edu>
2023-12-11 15:27:08 -0500

    Initial commit

---
+++ server.R
@@ -0,0 +1,11 @@
+# server for html_tests
+
+library(shiny)
+library(shinythemes)
+
+
+
+shinyServer(function(input, output, session){
+  
+})
+  
---
+++ www/mjscript.js
@@ -0,0 +1,11 @@
+<script type="text/x-mathjax-config">
+  MathJax.Hub.Config({
+    TeX: {
+      extensions: ["[Contrib]/mhchem/mhchem.js"]
+    }
+  });
+MathJax.Hub.Config({ 
+  "HTML-CSS": { scale: 90 }, 
+  SVG: { linebreaks: { automatic:true } }, 
+});
+</script>
